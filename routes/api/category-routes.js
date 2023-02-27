e264f9eb32b71339cfbd92ec5002cfe1066ea0e6
m1taylor <m1taylor@ryerson.ca>
2023-02-27 15:10:13 -0500

    add delete request for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -66,8 +66,21 @@ router.put('/:id', async (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryDestroy = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryDestroy) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    res.status(200).json(categoryDestroy);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
