5c983e8aa1f7cf19d693b789c41e2d375603b8aa
m1taylor <m1taylor@ryerson.ca>
2023-02-27 14:50:48 -0500

    added find all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,16 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll ({
+      include: [
+        { model: Product }
+      ],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500),json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
