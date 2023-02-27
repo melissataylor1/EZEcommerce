12e9299af45f417671a6a45d2fe923f585362c13
m1taylor <m1taylor@ryerson.ca>
2023-02-27 15:16:34 -0500

    added asyncs to requests

--- models/index.js
+++ models/index.js
@@ -4,6 +4,8 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+//SETTING UP BETWEEEN PRODUCT, CATEGORY, TAG, AND PRODUCTTAG MODELS
+
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,16 +4,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
+
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -89,7 +91,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
 });
 
