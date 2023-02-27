1e5e210bd821da9309dc4e103027f45665df43c1
m1taylor <m1taylor@ryerson.ca>
2023-02-27 15:18:33 -0500

    route handler for getting product data and error message

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [
+        { model: Category},
+        { model: Tag, through: ProductTag}
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
