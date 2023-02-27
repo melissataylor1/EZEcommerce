3346f86f27ead48d5cfe9d350486c93dac558679
m1taylor <m1taylor@ryerson.ca>
2023-02-27 14:13:35 -0500

    finished setting up fields/rules

--- models/Product.js
+++ models/Product.js
@@ -19,7 +19,29 @@ Product.init(
       type: DataTypes.STRING,
       allowNull: false
     },
+    price: {
+      type: DataTypes.DECIMAL, 
+      allowNull: false, 
+      validate: {
+        isDecimal: true 
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      defaultValue: [10],
+      validate: {
+        isNumeric: truec
+      }
+    },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id'
+    },
   }
+},
   {
     sequelize,
     timestamps: false,
