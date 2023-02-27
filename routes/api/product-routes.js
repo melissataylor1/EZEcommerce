9595461dc769eb1fc9bbe74c2e09d2f2a77ed978
m1taylor <m1taylor@ryerson.ca>
2023-02-27 15:23:21 -0500

    single product get route, for product id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
+      //including category and tag data
       include: [
         { model: Category},
         { model: Tag, through: ProductTag}
@@ -18,10 +19,24 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get one product
+// get single product by its id 
 router.get('/:id', async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      //including category and tag data
+      include: [
+        { model: Category },
+        { model: Tag, through: ProductTag }
+      ],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
